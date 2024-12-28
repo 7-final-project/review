000cbc53c7720b6cb0a97ec1d8f4de95449213d4
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-12-28 15:22:49 +0900

    [ chore ] 리뷰 생성 응답 더미 파라미터 수정 ( #1 )

--- src/main/java/com/qring/review/v1/controller/ReviewControllerV1.java
+++ src/main/java/com/qring/review/v1/controller/ReviewControllerV1.java
@@ -2,6 +2,7 @@ package com.qring.review.v1.controller;
 
 import com.qring.review.domain.v1.res.ResDTO;
 import com.qring.review.domain.v1.res.ReviewPostResDTOv1;
+import com.qring.review.infrastructure.docs.ReviewControllerSwagger;
 import com.qring.review.v1.req.PostReviewReqDTOv1;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ReviewControllerV1 {
+public class ReviewControllerV1 implements ReviewControllerSwagger {
 
     @PostMapping("/v1/reviews")
     public ResponseEntity<ResDTO<ReviewPostResDTOv1>> postBy(@RequestHeader("X-User-Id") Long userId,
@@ -22,7 +23,7 @@ public class ReviewControllerV1 {
                 ResDTO.<ReviewPostResDTOv1>builder()
                         .code(HttpStatus.CREATED.value())
                         .message("리뷰 생성에 성공했습니다.")
-                        .data(ReviewPostResDTOv1.of(1L, 9999L, 5, "example-content"))
+                        .data(ReviewPostResDTOv1.of(userId, 9999L, 5, "example-content"))
                         .build(),
                 HttpStatus.CREATED
         );
