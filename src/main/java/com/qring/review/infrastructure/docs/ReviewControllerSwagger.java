a3400ccece235f0468040c277246cfc100cfde70
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-12-28 15:30:16 +0900

    [ feat ] 리뷰 수정 API 스웨거 문서화 추가 ( #1 )

--- src/main/java/com/qring/review/infrastructure/docs/ReviewControllerSwagger.java
+++ src/main/java/com/qring/review/infrastructure/docs/ReviewControllerSwagger.java
@@ -3,6 +3,7 @@ package com.qring.review.infrastructure.docs;
 import com.qring.review.domain.v1.res.ResDTO;
 import com.qring.review.domain.v1.res.ReviewPostResDTOv1;
 import com.qring.review.v1.req.PostReviewReqDTOv1;
+import com.qring.review.v1.req.PutReviewReqDTOv1;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -11,9 +12,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Review", description = "생성, 조회, 검색, 수정, 삭제 관련 리뷰 API")
 public interface ReviewControllerSwagger {
@@ -25,4 +24,13 @@ public interface ReviewControllerSwagger {
     })
     @PostMapping("/v1/reviews")
     ResponseEntity<ResDTO<ReviewPostResDTOv1>> postBy(@RequestHeader("X-User-Id") Long userId, @Valid @RequestBody PostReviewReqDTOv1 dto);
+
+
+    @Operation(summary = "리뷰 수정", description = "사용자의 Id 와 식당의 Id 를 기준으로 리뷰를 수정하는 API 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "리뷰 수정 성공", content = @Content(schema = @Schema(implementation = ResDTO.class))),
+            @ApiResponse(responseCode = "400", description = "리뷰 수정 실패.", content = @Content(schema = @Schema(implementation = ResDTO.class)))
+    })
+    @PutMapping("/v1/reviews/{reviewId}")
+    ResponseEntity<ResDTO<Object>> putBy(@RequestHeader("X-User-Id") Long userId, @PathVariable Long reviewId, @Valid @RequestBody PutReviewReqDTOv1 dto);
 }
