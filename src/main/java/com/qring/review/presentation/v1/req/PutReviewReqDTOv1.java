e26825328b0ff11f999d5e9321a5646b74afdc3f
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-12-30 22:07:04 +0900

    [ refactor ] int 자료형 유효성 검사 어노테이션 변경 @NotNull -> @Positive ( #2 )

--- src/main/java/com/qring/review/presentation/v1/req/PutReviewReqDTOv1.java
+++ src/main/java/com/qring/review/presentation/v1/req/PutReviewReqDTOv1.java
@@ -1,10 +1,7 @@
 package com.qring.review.presentation.v1.req;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -19,7 +16,7 @@ public class PutReviewReqDTOv1 {
     @Getter
     public static class Review {
 
-        @NotNull(message = "평점을 입력해주세요.")
+        @Positive(message = "평점을 입력해주세요.")
         @Min(value = 1, message = "평점은 최소 1점부터입니다.")
         @Max(value = 5, message = "평점은 최대 5점까지입니다.")
         private int rating;
