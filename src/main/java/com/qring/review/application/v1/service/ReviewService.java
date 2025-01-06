b5d86a77fdfb5b5234af642c4870e84d26aaa887
Jun <dudwns1852@naver.com>
2025-01-07 01:18:01 +0900

    [ feat ] 리뷰 서비스 CRUD 구현 ( #7 )

--- src/main/java/com/qring/review/application/v1/service/ReviewService.java
+++ src/main/java/com/qring/review/application/v1/service/ReviewService.java
@@ -1,4 +1,73 @@
 package com.qring.review.application.v1.service;
 
+import com.qring.review.application.global.exception.EntityNotFoundException;
+import com.qring.review.application.v1.res.ReviewGetByIdResDTOV1;
+import com.qring.review.application.v1.res.ReviewPostResDTOV1;
+import com.qring.review.application.v1.res.ReviewSearchResDTOV1;
+import com.qring.review.domain.model.ReviewEntity;
+import com.qring.review.domain.repository.ReviewRepository;
+import com.qring.review.presentation.v1.req.PostReviewReqDTOV1;
+import com.qring.review.presentation.v1.req.PutReviewReqDTOV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class ReviewService {
+
+    private final ReviewRepository reviewRepository;
+
+    @Transactional
+    public ReviewPostResDTOV1 postBy(Long userId, PostReviewReqDTOV1 dto) {
+        // 리뷰 엔티티 생성
+        ReviewEntity reviewEntityForSave = ReviewEntity.createReviewEntity(
+                userId,
+                dto.getReview().getRestaurantId(),
+                dto.getReview().getRating(),
+                dto.getReview().getContent()
+                );
+
+        // 저장 및 DTO 반환
+        return ReviewPostResDTOV1.of(reviewRepository.save(reviewEntityForSave));
+    }
+
+    @Transactional(readOnly = true)
+    public ReviewSearchResDTOV1 searchBy(Pageable pageable, Long userId, Long restaurantId, String sort) {
+        return ReviewSearchResDTOV1.of(reviewRepository.findReviewPageByDeletedAtIsNullWithConditions(userId, restaurantId, sort, pageable));
+    }
+
+    @Transactional(readOnly = true)
+    public ReviewGetByIdResDTOV1 getBy(Long id) {
+        // 리뷰 조회
+        ReviewEntity reviewEntityForMapping = reviewRepository.findByIdAndDeletedAtIsNull(id)
+                .orElseThrow(() -> new EntityNotFoundException("리뷰를 찾을 수 없습니다."));
+        return ReviewGetByIdResDTOV1.of(reviewEntityForMapping);
+    }
+
+    @Transactional
+    public void putBy(Long userId, Long id, PutReviewReqDTOV1 dto) {
+        // 리뷰 엔티티 조회
+        ReviewEntity reviewEntityForModify = reviewRepository.findByIdAndDeletedAtIsNull(id)
+                .orElseThrow(() -> new EntityNotFoundException("리뷰를 찾을 수 없습니다."));
+
+        // 리뷰 수정
+        reviewEntityForModify.updateReviewEntity(
+                dto.getReview().getRating(),
+                dto.getReview().getContent()
+        );
+    }
+
+    @Transactional
+    public void deleteBy(Long userId, Long id) {
+        // 리뷰 엔티티 조회
+        ReviewEntity reviewEntityForDelete = reviewRepository.findByIdAndDeletedAtIsNull(id)
+                .orElseThrow(() -> new EntityNotFoundException("리뷰를 찾을 수 없습니다."));
+
+        // 리뷰 논리 삭제
+        reviewEntityForDelete.deleteReviewEntity(userId.toString());
+    }
 }
+
