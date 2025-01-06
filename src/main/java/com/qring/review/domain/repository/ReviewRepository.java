0d2c2760142c4661ac1b69f1629ed475304a8f29
Jun <dudwns1852@naver.com>
2025-01-07 00:48:35 +0900

    [ feat ] repository 생성 ( #7 )

---
+++ src/main/java/com/qring/review/domain/repository/ReviewRepository.java
@@ -0,0 +1,20 @@
+package com.qring.review.domain.repository;
+
+import com.qring.review.domain.model.ReviewEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ReviewRepository {
+
+    // 특정 ID로 삭제되지 않은 리뷰 조회
+    Optional<ReviewEntity> findByIdAndDeletedAtIsNull(Long id);
+
+    // 조건에 따른 리뷰 검색
+    Page<ReviewEntity> findReviewPageByDeletedAtIsNullWithConditions(Long userId, Long restaurantId, String sort, Pageable pageable);
+
+    // 리뷰 저장
+    ReviewEntity save(ReviewEntity categoryEntity);
+}
---
+++ src/main/java/com/qring/review/infrastructure/repository/ReviewRepositoryImpl.java
@@ -0,0 +1,37 @@
+package com.qring.review.infrastructure.repository;
+
+import com.qring.review.domain.model.ReviewEntity;
+import com.qring.review.domain.repository.ReviewRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class ReviewRepositoryImpl implements ReviewRepository {
+
+    private final JpaReviewRepository jpaReviewRepository;
+    private final ReviewQueryRepository reviewQueryRepository;
+
+    // 특정 ID로 삭제되지 않은 리뷰 조회
+    @Override
+    public Optional<ReviewEntity> findByIdAndDeletedAtIsNull(Long id) {
+        return jpaReviewRepository.findByIdAndDeletedAtIsNull(id);
+    }
+
+    // 조건에 따른 리뷰 검색
+    @Override
+    public Page<ReviewEntity> findReviewPageByDeletedAtIsNullWithConditions(Long userId, Long restaurantId, String sort, Pageable pageable) {
+        return reviewQueryRepository.findReviewPageByDeletedAtIsNullWithConditions(userId, restaurantId, sort, pageable);
+    }
+
+    // 리뷰 저장
+    @Override
+    public ReviewEntity save(ReviewEntity ReviewEntity) {
+        return jpaReviewRepository.save(ReviewEntity);
+    }
+}
