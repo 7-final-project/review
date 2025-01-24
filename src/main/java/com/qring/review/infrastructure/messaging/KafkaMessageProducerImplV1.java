1a01c4c4bed15f957e9c7b5286e4bd7a0aec13ce
오세창 <114675677+sepang-pang@users.noreply.github.com>
2025-01-24 22:02:31 +0900

    [ feat ] 카프카 토픽 환경변수 관리 ( #21 )

--- src/main/java/com/qring/review/infrastructure/messaging/KafkaMessageProducerImplV1.java
+++ src/main/java/com/qring/review/infrastructure/messaging/KafkaMessageProducerImplV1.java
@@ -3,6 +3,7 @@ package com.qring.review.infrastructure.messaging;
 import com.qring.review.application.message.KafkaMessageProducerV1;
 import com.qring.review.application.message.ReviewEventMessageDTOV1;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
@@ -12,11 +13,14 @@ public class KafkaMessageProducerImplV1 implements KafkaMessageProducerV1 {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
+    @Value("${spring.kafka.topic.review-event}")
+    private String reviewEventTopic;
+
     @Override
     public void publishReviewEvent(ReviewEventMessageDTOV1 reviewEventMessage) {
         // 토픽 이름: review-event-topic
         // 메시지 키(파티션 id로 사용됨): 식당 ID
         // 메시지: 리뷰 정보
-        kafkaTemplate.send("review-event-topic", String.valueOf(reviewEventMessage.getRestaurantId()), reviewEventMessage);
+        kafkaTemplate.send(reviewEventTopic, String.valueOf(reviewEventMessage.getRestaurantId()), reviewEventMessage);
     }
 }
